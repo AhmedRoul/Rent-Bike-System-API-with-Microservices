a8bd8176451e950673ea2ce72a4e165fdb236f49
AhmedRoul <{ID}+{username}@users.noreply.github.com>
2024-09-10 10:32:13 +0300

    first commit

---
+++ Rent Bike System API with Microservices/RentalRecordService/src/test/java/com/RentBikeSystem/RentalRecordService/RentalRecordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RentBikeSystem.RentalRecordService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalRecordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
