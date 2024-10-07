4c976d10dd91633c4c2755f24531aa1eaec81791
AhmedRoul <{ID}+{username}@users.noreply.github.com>
2024-10-08 00:10:35 +0300

    spring.boot.Message

--- Rent Bike System API with Microservices/RentalRecordService/src/main/java/com/RentBikeSystem/RentalRecordService/RentalRecordServiceApplication.java
+++ Rent Bike System API with Microservices/RentalRecordService/src/main/java/com/RentBikeSystem/RentalRecordService/RentalRecordServiceApplication.java
@@ -2,8 +2,10 @@ package com.RentBikeSystem.RentalRecordService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RentalRecordServiceApplication {
 
 	public static void main(String[] args) {
